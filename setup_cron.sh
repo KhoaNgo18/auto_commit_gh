39a810bb7924340fa61ebd4f80173785d2319b0a
khoango18 <khoango1810@gmail.com>
2025-02-14 21:26:28 +0700

    Update for auto pull

--- setup_cron.sh
+++ setup_cron.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
 # Path to the Python script
-SCRIPT_PATH="/home/ubuntu/khoa/others/auto_commit_gh/main.py"
-HOUR=20
+SCRIPT_PATH="/home/khoa/Khoa/my-project/auto-commit-github/main.py"
+HOUR=22
 MINUTE=00
 # Cron job command
 CRON_JOB="$MINUTE $HOUR * * * python3 $SCRIPT_PATH > auto_commit.log 2>&1"
 
-# Check existing cron jobs
-(crontab -l 2>/dev/null || true) | grep -q "$CRON_JOB"
-if [ $? -eq 0 ]; then
-    echo "Cron job is already set."
-else
-    # Add the cron job
-    (crontab -l 2>/dev/null; echo "$CRON_JOB") | crontab -
-    echo "Cron job has been added to run daily at $HOUR:$MINUTE."
-fi
+(crontab -l 2>/dev/null; echo "$CRON_JOB") | crontab -
+echo "Cron job has been added to run daily at $HOUR:$MINUTE."
+
+##
+HOUR=21
+MINUTE=55
+# Cron job command
+CRON_JOB="$MINUTE $HOUR * * * git pull"
+
+(crontab -l 2>/dev/null; echo "$CRON_JOB") | crontab -
+echo "Cron job has been added to run daily at $HOUR:$MINUTE."
